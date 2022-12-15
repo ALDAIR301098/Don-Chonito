867237f87cc2422f4fc67bd8b257128e7634e121
ALDAIR301098 <pv18021157@vallarta.tecmm.edu.mx>
2022-12-14 18:44:49 -0600

    new changes

--- database/db.js
+++ database/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
-const DB_HOST = process.env.DB_HOST || 'localhost'
-const BD_USER = process.env.DB_USER || 'bakeryAdmin'
-const DB_PASSWORD = process.env.DB_PASSWORD ||'pepito123'
-const DB_NAME = process.env.DB_DATABASE || 'bakery'
-const DB_PORT = process.env.DB_PORT || '3306'
+const DB_HOST = process.env.DB_HOST || 'containers-us-west-163.railway.app'
+const BD_USER = process.env.DB_USER || 'root'
+const DB_PASSWORD = process.env.DB_PASSWORD ||'9HhVXJlw4YBGv7Ad6Ph0'
+const DB_NAME = process.env.DB_DATABASE || 'railway'
+const DB_PORT = process.env.DB_PORT || '5542'
 
 const connection =  mysql.createConnection({
     host: DB_HOST,
