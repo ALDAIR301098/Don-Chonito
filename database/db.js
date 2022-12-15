0348db4ac5ac2cc81b34561dedd5c790c36d29b9
ALDAIR301098 <pv18021157@vallarta.tecmm.edu.mx>
2022-12-14 18:11:28 -0600

    UPDATE DATABASE DATA

--- database/db.js
+++ database/db.js
@@ -3,14 +3,14 @@ const mysql = require('mysql');
 const DB_HOST = process.env.DB_HOST || 'localhost'
 const BD_USER = process.env.DB_USER || 'bakeryAdmin'
 const DB_PASSWORD = process.env.DB_PASSWORD ||'pepito123'
-const DB_DATABASE = process.env.DB_DATABASE || 'bakery'
+const DB_NAME = process.env.DB_DATABASE || 'bakery'
 const DB_PORT = process.env.DB_PORT || '3306'
 
 const connection =  mysql.createConnection({
     host: DB_HOST,
     user: BD_USER,
     password: DB_PASSWORD,
-    database: DB_DATABASE,
+    database: DB_NAME,
     port: DB_PORT
 });
 
